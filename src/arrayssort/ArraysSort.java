3af07821851464a1adf45652e459520642df2530
pourya-siyami <78645842+pourya-siyami@users.noreply.github.com>
2021-02-11 17:43:37 +0330

    Second commit

--- src/arrayssort/ArraysSort.java
+++ src/arrayssort/ArraysSort.java
@@ -4,9 +4,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArraysSort {
+    // Return the number of swaps to sort the array
+    public int minSwaps(int[] num, int N) {
+        int ans = 0;
+        int[] temp = Arrays.copyOfRange(num, 0, N);
+        Arrays.sort(temp);
+        for (int i = 0; i < N; i++) {
+            if (num[i] != temp[i]) {
+                ans++;
+                swap(num, i, indexOf(num, temp[i]));
+            }
+        }
+        return ans;
+    }
+
+    public void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
+    public int indexOf(int[] num, int n) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int n;
-        int count = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter number of array elements:");
         n = input.nextInt();
@@ -15,22 +43,8 @@ public class ArraysSort {
         for (int i = 0; i < n; i++) {
             num[i] = input.nextInt();
         }
-        System.out.println("entered array is: " + Arrays.toString(num));
-
         System.out.println("---------------------------");
+        System.out.println("Number of swaps to sort: " + new ArraysSort().minSwaps(num, n));
 
-        //sort //swap
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (num[j] > num[j + 1]) {
-                    int tNum = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = tNum;
-                    count += 1;
-                }
-            }
-        }
-        System.out.println("sorted array is: " + Arrays.toString(num));
-        System.out.println("Number of shifts: "+ count);
     }
 }
